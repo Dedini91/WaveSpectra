ac28087688d04fd4aa5bf93807068771175043fa
Dedini91 <131020229+Dedini91@users.noreply.github.com>
2023-05-10 11:34:32 +1000

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -15,11 +15,10 @@ import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 from prettytable import PrettyTable
 from torchinfo import summary
-
-from utils.ssim import SSIM
 from utils.utils import *
 from models.WaveNet import WaveNet
 from utils.dataset import WaveSpectra
+from torchmetrics import StructuralSimilarityIndexMeasure, CosineSimilarity, MeanAbsoluteError
 
 warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
 
@@ -29,19 +28,15 @@ parser = argparse.ArgumentParser(description="Options for model evaluation")
 parser.add_argument("--verbose", action="store_true", default=False,
                     help="verbose output")
 parser.add_argument("--model_path", action="store", type=str, required=True,
-                    help="path to model .pth file")
+                    help="path to model and optimiser state_dict.pth files")
 parser.add_argument("--img_path", action="store", type=str, required=True,
                     help="path to source images for evaluation and inference")
 parser.add_argument("--target_path", action="store", type=str, required=True,
                     help="path to target images for evaluation and inference")
 parser.add_argument("--device", type=str, default='cuda', choices=['cuda', 'cpu'],
                     help="device")
-parser.add_argument("--errmaps", action='store_false', default=True,
-                    help="turns off error maps in output files (may speed up evaluation)")
-parser.add_argument("-l", "--loss", action="store", type=str, default='ssim', choices=['l1', 'mse', 'huber', 'cosine', 'ssim'],
-                    help="loss function")
-parser.add_argument("--reduction", action="store", type=str, default='sum', choices=['mean', 'sum', 'none'],
-                    help="reduction method")
+parser.add_argument("--save_preds", action="store_false", default=True,
+                    help="pass argument to prevent saving of predictions")
 
 args = vars(parser.parse_args())
 
@@ -94,6 +89,8 @@ if device == 'cuda':
     torch.backends.cudnn.benchmark = True
 if args['verbose']:
     log.info("\nDevice:\t" + str(device))
+print(args['img_path'])
+print(args['target_path'])
 
 # Get all image paths
 x_test_paths = glob(str(args['img_path']) + '/*.jpg')
@@ -117,7 +114,7 @@ if args['verbose']:
     log.debug(summary(network, (1, 1, 64, 64), verbose=2))
 
 for param in network.parameters():
-    param.requires_grad = False
+    param.requires_grad = True
 
 total_samples = len(test)
 n_iter = math.ceil(total_samples / 1)
@@ -128,56 +125,35 @@ if args['verbose']:
     log.info("# iterations: " + str(n_iter))
 
 log.info("==========================================================================================")
-# Set up loss function based on user-specified parameters
-if args['loss'] == 'l1':
-    loss_function = torch.nn.L1Loss(reduction=str(args['reduction']))
-    torch.nn.L1Loss()
-if args['loss'] == 'mse':
-    loss_function = torch.nn.MSELoss(reduction=(str(args['reduction'])))
-if args['loss'] == 'huber':
-    loss_function = torch.nn.HuberLoss(reduction=str(args['reduction']))
+ssim = StructuralSimilarityIndexMeasure(reduction='elementwise_mean')
+cos = CosineSimilarity(reduction='mean')
+mae = MeanAbsoluteError()
+l1 = torch.nn.L1Loss(reduction='sum')
 
 log.info('Setup complete!')
 log.info("==========================================================================================")
 log.info("==========================================================================================")
 
-image_id, dimensions, l1_sum, l1_mean, l2_sum, l2_mean, huber_sum, huber_mean = \
-    [], [], [], [], [], [], [], [],
-cosineLoss = []
-cosineSim = []
-ssimLoss = []
-ssimSim = []
+ssimLoss, ssimSim = [], []
+cosineLoss, cosineSim = [], []
+image_id, dimensions, l1_sum = [], [], []
+
 
 def evaluate():
-    image_id.clear()
-    l1_sum.clear()
-    l1_mean.clear()
-    l2_sum.clear()
-    l2_mean.clear()
-    huber_sum.clear()
-    huber_mean.clear()
-    cosineLoss.clear()
-    cosineSim.clear()
-    ssimLoss.clear()
-    ssimSim.clear()
     log.info("Evaluating model...")
 
     model = WaveNet()
     model.load_state_dict(torch.load(str(args['model_path'])))
-    
     if args['verbose']:
         log.info("Loading model from supplied model_path: {}".format(str(args['model_path']).replace("\\", '/')))
-        
+
     model.to(device)
     model.eval()
 
-    cos_test_err, cos_test_sim, ssim_test_err, ssim_test_sim = [], [], [], []
-    
     with tqdm(test_loader, unit="batch") as tepoch:
         with torch.no_grad():
-            ssim = SSIM().cuda() if torch.cuda.is_available() else SSIM()
             i = 0
-            for data, target, data_index, target_index in tepoch:
+            for data, target, data_index, target_index, data_orig in tepoch:
                 pred_path = Path.joinpath(eval_path, str(data_index[0]).split('.')[0])
                 pred_path.mkdir(parents=True, exist_ok=True)
                 data = data.to(device)
@@ -185,77 +161,59 @@ def evaluate():
                 tepoch.set_description(f"Evaluating")
                 output = model(data)
 
-                test_l1 = F.l1_loss(output, target, reduction='sum')
-                test_mse = F.mse_loss(output, target, reduction='sum')
-                test_huber = F.huber_loss(output, target, reduction='sum')
-                
-                # Flatten images -> 1D tensors: Compute cosine similarity/loss
-                target_vec = torch.flatten(target)
-                output_vec = torch.flatten(output)
-                cosine_sim = F.cosine_similarity(output_vec, target_vec, dim=0)  # Calculate loss
-                cosine_loss = 1 - cosine_sim
+                # Compute cosine similarity:
+                target_vec = torch.flatten(target, start_dim=1, end_dim=3)
+                output_vec = torch.flatten(output, start_dim=1, end_dim=3)
+                cosine_similarity = cos(output_vec, target_vec)  # Calculate loss
+                cosine_loss = 1 - cosine_similarity
 
                 # Compute SSIM
-                ssim_similarity = ssim(target, output)
-                ssim_loss_initial = 1 - ssim_similarity
+                ssim_similarity = ssim(output, target)
+                ssim_loss = 1 - ssim_similarity
 
-                if args['loss'] == 'cosine':
-                    loss = cosine_loss.item()
-                elif args['loss'] == 'ssim':
-                    loss = ssim_loss_initial
-                else:
-                    loss = loss_function(output, target)  # Calculate loss
+                # Compute l1 loss
+                l1_loss = l1(output, target)
 
-                cos_test_sim.append(cosine_sim.item() * 100)
-                cos_test_err.append(cosine_loss.item())
-                ssim_test_sim.append(ssim_similarity * 100)
-                ssim_test_err.append(ssim_loss_initial.item())
-
-                if args['errmaps']:
-                    plot_l1 = F.l1_loss(output, target, reduction='none').detach().cpu().numpy().squeeze()
-                    plot_l2 = F.mse_loss(output, target, reduction='none').detach().cpu().numpy().squeeze()
-                    plot_huber = F.huber_loss(output, target, reduction='none').detach().cpu().numpy().squeeze()
-                    plot_cos_loss = cosine_loss.detach().cpu().numpy().squeeze()
-                    plot_ssim_loss = ssim_loss_initial.detach().cpu().numpy().squeeze()
+                l1_sum.append(l1_loss.item())
+                cosineLoss.append(cosine_loss.item())
+                ssimLoss.append(ssim_loss.item())
+                cosineSim.append(cosine_similarity.item())
+                ssimSim.append(ssim_similarity.item())
 
                 prediction = output.detach().cpu().numpy().squeeze()
 
-                save_eval(data.detach().cpu().numpy().squeeze(),
-                          target.detach().cpu().numpy().squeeze(),
-                          prediction,
-                          torch.Tensor.tolist(test_l1),
-                          torch.Tensor.tolist(test_mse),
-                          torch.Tensor.tolist(test_huber),
-                          data_index[0],
-                          pred_path)
+                if args['save_preds']:
+                    save_eval(data_orig.detach().cpu().numpy().squeeze(),
+                              target.detach().cpu().numpy().squeeze(),
+                              prediction,
+                              l1_sum[-1],
+                              ssimLoss[-1],
+                              cosineLoss[-1],
+                              data_index[0],
+                              pred_path)
+
                 i += 1
 
-                if args['errmaps']:
-                    save_error(target.detach().cpu().numpy().squeeze(),
-                               prediction,
-                               plot_l1,
-                               plot_l2,
-                               plot_huber,
-                               data_index[0],
-                               pred_path)
-
                 image_id.append(str(data_index[0]))
-                l1_sum.append(torch.Tensor.tolist(F.l1_loss(output, target, reduction='sum')))
-                l1_mean.append(torch.Tensor.tolist(F.l1_loss(output, target, reduction='mean')))
-                l2_sum.append(torch.Tensor.tolist(F.mse_loss(output, target, reduction='sum')))
-                l2_mean.append(torch.Tensor.tolist(F.mse_loss(output, target, reduction='mean')))
-                huber_sum.append(torch.Tensor.tolist(F.huber_loss(output, target, reduction='sum')))
-                huber_mean.append(torch.Tensor.tolist(F.huber_loss(output, target, reduction='mean')))
-                
-                cosineSim.append(torch.Tensor.tolist(cosine_sim))
-                cosineLoss.append(torch.Tensor.tolist(cosine_loss))
 
-                ssimLoss.append(torch.Tensor.tolist(ssim_loss_initial))
-                ssimSim.append(torch.Tensor.tolist(ssim_similarity))
+            results = PrettyTable(["L1 Loss",
+                                   " |",
+                                   "Cosine Loss",
+                                   "Cosine Similarity",
+                                   '| ',
+                                   "SSIM Loss",
+                                   "SSIM Similarity"])
+            results.add_rows([['{:.4f}'.format(sum(l1_sum) / len(l1_sum)),
+                               ' |',
+                               '{:.4f}'.format(sum(cosineLoss) / len(cosineLoss)),
+                               '{:.4f}'.format(sum(cosineSim) / len(cosineSim)),
+                               '| ',
+                               '{:.4f}'.format(sum(ssimLoss) / len(ssimLoss)),
+                               '{:.4f}'.format(sum(ssimSim) / len(ssimSim))]])
 
-                # test_losses_l1.append(torch.Tensor.tolist(test_l1))
-                # test_losses_mse.append(torch.Tensor.tolist(test_mse))
-                # test_losses_huber.append(torch.Tensor.tolist(test_huber))
+            results.border = False
+            log.info(results)
+            log.info("\n")
 
     return None
 
@@ -263,24 +221,20 @@ def evaluate():
 def losses_to_csv(name):
     loss_dict = {
         "Image ID": image_id,
-        "L1/MAE (sum)": l1_sum,
-        "L1/MAE (mean)": l1_mean,
-        "L2/MSE (sum)": l2_sum,
-        "L2/MSE (mean)": l2_mean,
-        "Huber (sum)": huber_sum,
-        "Huber (mean)": huber_mean,
+        "L1/MAE": l1_sum,
         "Cosine error": cosineLoss,
         "Cosine similarity": cosineSim,
         "SSIM error": ssimLoss,
         "SSIM similarity": ssimSim
     }
     df = DataFrame.from_dict(loss_dict)
-    df = df.sort_values(by='L1/MAE (sum)', ascending=True)
+    df = df.sort_values(by='L1/MAE', ascending=True)
     log.info(name + ":")
     log.info(df)
 
     df.to_csv(str(metrics_path) + "/" + name + ".csv", sep=",", float_format="%.6f", header=True, index=False)
-    return None
+    log.info("Saved as " + str(metrics_path) + "/" + name + ".csv")
+
 
 # Execute program
 evaluate()
