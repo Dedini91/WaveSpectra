a49a8250c0a05270e607d3c53a7412b3be566826
Dedini91 <131020229+Dedini91@users.noreply.github.com>
2023-05-10 13:43:06 +1000

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -12,10 +12,6 @@ def sigmoid(x):
     return 1 / (1 + torch.exp(-8 * x + 4.5))
 
 
-def get_mean(losses):
-    return sum(losses) / len(losses)
-
-
 def getinfo(img):
     (h, w) = img.shape
     total_px = img.shape[0] * img.shape[1]
@@ -48,12 +44,25 @@ def normalise_to_source(target, output):
 
 def save_sample(data, target, prediction, epoch, filename, root_path):
     epoch_str = str(epoch + 1).zfill(3)
-    plt.subplot(131, title='Source', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (1, 4))
+    plt.title("Source")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(data[0, :, :].squeeze(), cmap="gray")
-    plt.subplot(133, title='Target', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (2, 3))
+    plt.title("Target")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(target[0, :, :].squeeze(), cmap="gray")
-    plt.subplot(132, title='Prediction', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (5, 6))
+    plt.title("Prediction")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(prediction[:, :].squeeze(), cmap="gray")
+
     plt.suptitle("Output: Epoch " + str(epoch + 1))
     plt.tight_layout()
 
@@ -74,55 +83,95 @@ def save_sample(data, target, prediction, epoch, filename, root_path):
 def save_examples(x_data, y_data, z_data, mode, epoch, filename, root_path, batch):
 
     if batch > 1:
-      for i in range(len(filename)):
-          plt.subplot(131, title='Source', xticks=[], yticks=[])
-          plt.imshow(x_data[i, :, :].squeeze(), cmap="gray")
-          plt.subplot(132, title='Target', xticks=[], yticks=[])
-          plt.imshow(y_data[i, :, :].squeeze(), cmap="gray")
-          plt.subplot(133, title='Prediction', xticks=[], yticks=[])
-          plt.imshow(z_data[i, :, :].squeeze(), cmap="gray")
-          plt.suptitle(mode + " output: Epoch " + str(epoch + 1))
-          plt.tight_layout()
-          if mode == 'Training':
-              plt.savefig(str(root_path) + "/predictions/training/epoch_" + str(epoch+1) + "/" + filename[i])
-          elif mode == 'Validation':
-              plt.savefig(str(root_path) + "/predictions/validation/epoch_" + str(epoch+1) + "/" + filename[i])
-          plt.close()
+        for i in range(len(filename)):
+            plt.subplot(2, 3, (1, 4))
+            plt.title("Source")
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(x_data[i, :, :].squeeze(), cmap="gray")
+
+            plt.subplot(2, 3, (2, 3))
+            plt.title("Target")
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(y_data[i, :, :].squeeze(), cmap="gray")
+
+            plt.subplot(2, 3, (5, 6))
+            plt.title("Prediction")
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(z_data[i, :, :].squeeze(), cmap="gray")
+
+            plt.suptitle(mode + " output: Epoch " + str(epoch + 1))
+            # plt.tight_layout()
+            if mode == 'Training':
+                plt.savefig(str(root_path) + "/predictions/training/epoch_" + str(epoch+1) + "/" + filename[i])
+            elif mode == 'Validation':
+                plt.savefig(str(root_path) + "/predictions/validation/epoch_" + str(epoch+1) + "/" + filename[i])
+            plt.close()
     else:
-      plt.subplot(131, title='Source', xticks=[], yticks=[])
-      plt.imshow(x_data[:, :], cmap="gray")
-      plt.subplot(132, title='Target', xticks=[], yticks=[])
-      plt.imshow(y_data[:, :], cmap="gray")
-      plt.subplot(133, title='Prediction', xticks=[], yticks=[])
-      plt.imshow(z_data[:, :, :].squeeze(), cmap="gray")
-      plt.suptitle(mode + " output: Epoch " + str(epoch + 1))
-      plt.tight_layout()
-      if mode == 'Training':
-          plt.savefig(str(root_path) + "/predictions/training/epoch_" + str(epoch+1) + "/" + filename[0])
-      elif mode == 'Validation':
-          plt.savefig(str(root_path) + "/predictions/validation/epoch_" + str(epoch+1) + "/" + filename[0])
-      plt.close()
+        plt.subplot(2, 3, (1, 4))
+        plt.title("Source")
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(x_data[:, :], cmap="gray")
+
+        plt.subplot(2, 3, (2, 3))
+        plt.title("Target")
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(y_data[:, :], cmap="gray")
+
+        plt.subplot(2, 3, (5, 6))
+        plt.title("Prediction")
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(z_data[:, :, :].squeeze(), cmap="gray")
+
+        plt.suptitle(mode + " output: Epoch " + str(epoch + 1))
+
+        if mode == 'Training':
+            plt.savefig(str(root_path) + "/predictions/training/epoch_" + str(epoch+1) + "/" + filename[0])
+        elif mode == 'Validation':
+            plt.savefig(str(root_path) + "/predictions/validation/epoch_" + str(epoch+1) + "/" + filename[0])
+        plt.close()
+
     return None
 
 
-def save_inference(x_data, y_data, z_data, loss1, loss2, loss3, filename, root_path):
-    plt.subplot(131, title='Source', xticks=[], yticks=[])
+def save_inference(x_data, y_data, z_data, loss1, ssim, cosine, filename, root_path):
+    plt.subplot(2, 3, (1, 4))
+    plt.title("Source")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(x_data[0, :, :].squeeze(), cmap="gray")
-    plt.subplot(132, title='Target', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (2, 3))
+    plt.title("Target")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(y_data[0, :, :].squeeze(), cmap="gray")
-    plt.subplot(133, title='Prediction', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (5, 6))
+    plt.title("Prediction")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(z_data[:, :], cmap="gray")
-    plt.suptitle("Evaluation:\nL1(MAE)/"
+
+    plt.suptitle("Evaluation:\nL1(MAE): "
                  + str(round(loss1, 4))
-                 + "\nL2(MSE)/" + str(round(loss2, 4))
-                 + "\nHuber/" + str(round(loss3, 4)))
-    plt.tight_layout()
+                 + "/SSIM: "
+                 + str(round(ssim, 4))
+                 + "/Cosine: "
+                 + str(round(cosine, 4)))
+
     plt.savefig(str(root_path) + "/predictions/evaluation/" + str(filename[0]))
     plt.close()
+
     return None
 
 
-def save_eval(x_data, y_data, z_data, loss1, loss2, loss3, filename, root_path):
+def save_eval(x_data, y_data, z_data, loss1, ssim, cosine, filename, root_path):
     plt.imshow(x_data[:, :], cmap='gray')
     plt.axis('off')
     plt.tight_layout()
@@ -141,39 +190,33 @@ def save_eval(x_data, y_data, z_data, loss1, loss2, loss3, filename, root_path):
     plt.savefig(str(root_path) + "/" + str(filename).split(".")[0] + "_pred.jpg")
     plt.close()
 
-    plt.subplot(131, title='Source', xticks=[], yticks=[])
+    plt.subplot(2, 3, (1, 4))
+    plt.title("Source")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(x_data[:, :], cmap="gray")
-    plt.subplot(132, title='Target', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (2, 3))
+    plt.title("Target")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(y_data[:, :], cmap="gray")
-    plt.subplot(133, title='Prediction', xticks=[], yticks=[])
+
+    plt.subplot(2, 3, (5, 6))
+    plt.title("Prediction")
+    plt.xticks([])
+    plt.yticks([])
     plt.imshow(z_data[:, :].squeeze(), cmap="gray")
-    plt.suptitle("Evaluation:\nL1(MAE)/"
+
+    plt.suptitle("Evaluation:\nL1(MAE): "
                  + str(round(loss1, 4))
-                 + "\nL2(MSE)/" + str(round(loss2, 4))
-                 + "\nHuber/" + str(round(loss3, 4)))
-    plt.tight_layout()
+                 + "/SSIM: "
+                 + str(round(ssim, 4))
+                 + "/Cosine: "
+                 + str(round(cosine, 4)))
+
     plt.savefig(str(root_path) + "/" + str(filename).split(".")[0] + "_comp.jpg")
     plt.close()
-    return None
-
-
-def save_error(target, prediction, error1, error2, error3, filename, root_path):
-    plt.subplot(221, title='Target', xticks=[], yticks=[])
-    plt.imshow(target[:, :], cmap="gray")
-    plt.subplot(222, title='Prediction', xticks=[], yticks=[])
-    plt.imshow(prediction[:, :], cmap="gray")
-
-    plt.subplot(234, title='L1/MAE', xticks=[], yticks=[])
-    plt.imshow(error1[:, :], cmap="jet")
-    plt.subplot(235, title='L2/MSE', xticks=[], yticks=[])
-    plt.imshow(error2[:, :], cmap="jet")
-    plt.subplot(236, title='Huber', xticks=[], yticks=[])
-    plt.imshow(error3[:, :], cmap="jet")
-
-    plt.suptitle("Error maps for three losses:")
-    plt.savefig(str(root_path) + "/" + str(filename).split(".")[0] + "_error.jpg")
-    plt.close()
-
     return None
 
 
@@ -181,14 +224,106 @@ def plot_losses(metrics_path, train_losses, val_losses):
     xs = [x for x in range(len(train_losses))]
     plt.plot(xs, train_losses, color='blue', label='Train')
     plt.plot(xs, val_losses, color='red', label='Validation')
-    plt.title("Train/Val losses")
+    plt.title("Train/Validation losses")
     plt.xlabel("Epochs")
     plt.xticks(range(0, len(xs)))
     plt.ylabel("Loss")
     plt.tight_layout()
     plt.legend()
-    plt.savefig(str(metrics_path) + "/losses.jpg")
+    plt.savefig(str(metrics_path) + "/metrics_losses.jpg")
+    plt.close()
+    return None
 
+
+def plot_l1_losses(metrics_path, l1_training_losses, l1_validation_losses):
+    xs = [x for x in range(len(l1_training_losses))]
+    plt.plot(xs, l1_training_losses, color='blue', label='Train')
+    plt.plot(xs, l1_validation_losses, color='red', label='Validation')
+    plt.title("L1 loss")
+    plt.xlabel("Epochs")
+    plt.xticks(range(0, len(xs)))
+    plt.ylabel("Loss")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(str(metrics_path) + "/metrics_losses_l1.jpg")
+    plt.close()
+    return None
+
+
+def plot_cosine_losses(metrics_path, cosine_training_losses, cosine_validation_losses):
+    xs = [x for x in range(len(cosine_training_losses))]
+    plt.plot(xs, cosine_training_losses, color='blue', label='Train')
+    plt.plot(xs, cosine_validation_losses, color='red', label='Validation')
+    plt.title("Cosine loss")
+    plt.xlabel("Epochs")
+    plt.xticks(range(0, len(xs)))
+    plt.ylabel("Loss")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(str(metrics_path) + "/metrics_losses_cosine.jpg")
+    plt.close()
+    return None
+
+
+def plot_ssim_losses(metrics_path, ssim_training_losses, ssim_validation_losses):
+    xs = [x for x in range(len(ssim_training_losses))]
+    plt.plot(xs, ssim_training_losses, color='blue', label='Train')
+    plt.plot(xs, ssim_validation_losses, color='red', label='Validation')
+    plt.title("SSIM loss")
+    plt.xlabel("Epochs")
+    plt.xticks(range(0, len(xs)))
+    plt.ylabel("Loss")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(str(metrics_path) + "/metrics_losses_ssim.jpg")
+    plt.close()
+    return None
+
+
+def plot_cosine_similarities(metrics_path, cosine_training_similarities, cosine_validation_similarities):
+    xs = [x for x in range(len(cosine_training_similarities))]
+    plt.plot(xs, cosine_training_similarities, color='blue', label='Train')
+    plt.plot(xs, cosine_validation_similarities, color='red', label='Validation')
+    plt.title("Cosine similarity")
+    plt.xlabel("Epochs")
+    plt.xticks(range(0, len(xs)))
+    plt.ylabel("Similarity")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(str(metrics_path) + "/metrics_similarity_cosine.jpg")
+    plt.close()
+    return None
+
+
+def plot_ssim_similarities(metrics_path, ssim_training_similarities, ssim_validation_similarities):
+    xs = [x for x in range(len(ssim_training_similarities))]
+    plt.plot(xs, ssim_training_similarities, color='blue', label='Train')
+    plt.plot(xs, ssim_validation_similarities, color='red', label='Validation')
+    plt.title("SSIM similarity")
+    plt.xlabel("Epochs")
+    plt.xticks(range(0, len(xs)))
+    plt.ylabel("Similarity")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(str(metrics_path) + "/metrics_similarity_ssim.jpg")
+    plt.close()
+    return None
+
+
+def plot_final_losses(metrics_path, train_losses, val_losses, epoch):
+    xs = [x for x in range(len(train_losses))]
+    plt.plot(xs, train_losses, color='blue', label='Train')
+    plt.plot(xs, val_losses, color='red', label='Validation')
+    plt.title("Train/Val losses")
+    plt.xlabel("Epochs")
+    plt.xticks(range(0, len(xs)))
+    plt.ylabel("Loss")
+    if epoch != 0:
+        plt.axvline(x=epoch, color='purple', label='L1 loss added', ls=':')
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(str(metrics_path) + "/metrics_losses.jpg")
+    plt.close()
     return None
 
 
